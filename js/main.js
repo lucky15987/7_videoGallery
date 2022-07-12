e4fe7d262fd9b52da5954691931d93eec6085c1f
Ethan N <lucky35789@yahoo.com>
2022-07-12 15:37:32 -0400

    add js functions for video player

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 var videoPlayer = document.getElementById("videoPlayer");
 var vmyVideo = document.getElementById("myVideo");
+
+var closebtn = document.getElementById("close-btn");
+
+
+
+function stopVideo() {
+    videoPlayer.style.display = "none";
+}
+
+closebtn.addEventListener("click", stopVideo);
